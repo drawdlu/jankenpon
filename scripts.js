2cb517df7243002352254e630a39ff94edf4333f
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-08-29 20:00:55 +0800

    Generate a rock choice based on randomNumber 1-100

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+// Creates a random choice of rock, paper or scissors
+function getComputerChoice() {
+    // Get a random number from 1-100
+    let randomNumber = Math.round(Math.random() * 100) + 1;
+
+    // Less than or equal to 33 means rock
+    if ( randomNumber <= 33 ) {
+        return "rock"
+    }
+
+}
